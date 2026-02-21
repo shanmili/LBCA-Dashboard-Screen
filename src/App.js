5954e651ff39b716471aeed22e67153ecb00813a
Keybird Evasco <knamitevasco@gmail.com>
2026-02-21 22:11:39 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DashboardScreen from './screens/DashboardScreen';
+import AccountSettings from './components/layout/TopNavBar/Accountsettings';
 
 function App() {
-  return <DashboardScreen />;
+  return (
+    <BrowserRouter basename="/LBCA-Monitoring-System">
+      <Routes>
+        <Route path="/" element={<DashboardScreen />} />
+        <Route path="/settings" element={<AccountSettings />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
